49295e56410d3f1d93396aa1cdb4c9cd6752efc4
Jeemin Jeong <jeemin.jeong@relativity.com>
2024-03-04 11:15:04 -0600

    testing

---
+++ ConsoleApp3.Tests/Test.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp3.Tests
+{
+	[SetUpFixture]
+	public class Test
+	{
+		[SetUp]
+		public void Setup()
+		{
+			Environment.SetEnvironmentVariable("ClientId", "TestClientId");
+			Environment.SetEnvironmentVariable("ClientSecret", "TestClientSecret");
+			Environment.SetEnvironmentVariable("ClientTenantId", "TestClientTenantId");
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			Environment.SetEnvironmentVariable("ClientId", null);
+			Environment.SetEnvironmentVariable("ClientSecret", null);
+			Environment.SetEnvironmentVariable("ClientTenantId", null);
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.That(Environment.GetEnvironmentVariable("ClientId"), Is.Not.Null.Or.Empty);
+		}
+	}
+}
